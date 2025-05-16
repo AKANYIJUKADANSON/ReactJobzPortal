a53b2f360e6a7fb86c94aedf249c9cb218b3807a
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2025-05-16 15:21:43 +0300

    Creating pages

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { TiLocation } from 'react-icons/ti';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -37,11 +37,11 @@ const JobListing = ({ job }) => {
             <TiLocation className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
-          <a
+          <Link
             to={`/jobs/${job.id}`}
             className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
